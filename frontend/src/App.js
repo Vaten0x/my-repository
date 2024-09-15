2ae81101a8d25800b0e23c033e29fba6c0b020a3
Jason Lee <113562298+Vaten0x@users.noreply.github.com>
2024-09-15 12:07:01 -0700

    Changed for logging errors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,42 +8,47 @@ function App() {
   const [results, setResults] = useState('');
 
   const handleInputChange = (event) => {
+    console.log("Input change detected:", event.target.value); // Log when input changes
     setKeyword(event.target.value);
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    console.log("Form submitted with keyword:", keyword); // Log the keyword on form submission
+
     axios.post('https://sentimentscanner-c0bbd127ef8e.herokuapp.com/analyze', { keyword })
       .then(response => {
+        console.log("Response received:", response); // Log the full response from the server
         setResults(response.data.reddit_df);
       })
       .catch(error => {
-        console.error('There was an error!', error);
+        console.error('There was an error!', error); // Log any errors during the request
       });
   };
 
   return (
     <div className="App">
-    <SwirlBackground />
-    <header className="App-header">
-      <h1>SentimentScanner</h1>
-      <form onSubmit={handleSubmit} className="input-form">
-      <textarea
-        value={keyword}
-        onChange={handleInputChange}
-        placeholder="Enter Text Here"
-        className="input-box"
-        rows="1" // Set an initial number of rows
-        onInput={(e) => {
-          e.target.style.height = 'auto'; // Reset the height
-          e.target.style.height = `${e.target.scrollHeight}px`; // Set height to scroll height
-        }}
-      />
-        <button type="submit" className="submit-button">Analyze</button>
-      </form>
-      <div className="results" dangerouslySetInnerHTML={{ __html: results }} />
-    </header>
-  </div>
+      <SwirlBackground />
+      <header className="App-header">
+        <h1>SentimentScanner</h1>
+        <form onSubmit={handleSubmit} className="input-form">
+          <textarea
+            value={keyword}
+            onChange={handleInputChange}
+            placeholder="Enter Text Here"
+            className="input-box"
+            rows="1"
+            onInput={(e) => {
+              console.log("Adjusting textarea height"); // Log when the textarea height adjusts
+              e.target.style.height = 'auto'; // Reset the height
+              e.target.style.height = `${e.target.scrollHeight}px`; // Set height to scroll height
+            }}
+          />
+          <button type="submit" className="submit-button">Analyze</button>
+        </form>
+        <div className="results" dangerouslySetInnerHTML={{ __html: results }} />
+      </header>
+    </div>
   );
 }
 
